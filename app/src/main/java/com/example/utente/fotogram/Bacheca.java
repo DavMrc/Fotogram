98e973a0406eb2cdbfa46b8c2857ad2f4d6fb05c
DavMrc <45066379+DavMrc@users.noreply.github.com>
2018-11-24 17:22:15 +0100

    added Fragments added footer

---
+++ app/src/main/java/com/example/utente/fotogram/Bacheca.java
@@ -0,0 +1,82 @@
+package com.example.utente.fotogram;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Bacheca extends AppCompatActivity {
+
+    final Fragment bacheca= new BachecaFragment();
+    final Fragment cerca= new RicercaFragment();
+    final Fragment nuovo_post= new Nuovo_Post_Fragment();
+    final Fragment profilo= new ProfiloFragment();
+
+    final FragmentManager fManager= getSupportFragmentManager();
+    Fragment active= bacheca;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacheca);
+
+        hideBottomNavBar();
+        setupFooter();
+    }
+
+    @Override
+    protected void onResume() {
+        hideBottomNavBar();
+        super.onResume();
+    }
+
+    private void hideBottomNavBar(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+    }
+
+    private void setupFooter(){
+        //aggiunge i fragment nascondendo quelli non utilizzati
+        fManager.beginTransaction().add(R.id.main_container, bacheca, "1").commit();
+        fManager.beginTransaction().add(R.id.main_container, cerca, "2").hide(cerca).commit();
+        fManager.beginTransaction().add(R.id.main_container, nuovo_post, "3").hide(nuovo_post).commit();
+        fManager.beginTransaction().add(R.id.main_container, profilo, "4").hide(profilo).commit();
+
+//        assegna ad ogni elemento del nav footer un click event
+        BottomNavigationView footer= findViewById(R.id.footer);
+        footer.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.bacheca:
+                        fManager.beginTransaction().hide(active).show(bacheca).commit();
+                        active= bacheca;
+                        break;
+                    case R.id.cerca:
+                        fManager.beginTransaction().hide(active).show(cerca).commit();
+                        active= cerca;
+                        break;
+                    case R.id.nuovo:
+                        fManager.beginTransaction().hide(active).show(nuovo_post).commit();
+                        active= nuovo_post;
+                        break;
+                    case R.id.profilo:
+                        fManager.beginTransaction().hide(active).show(profilo).commit();
+                        active= profilo;
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/utente/fotogram/Login.java
+++ app/src/main/java/com/example/utente/fotogram/Login.java
@@ -1,7 +1,16 @@
 package com.example.utente.fotogram;
 
+import android.content.Intent;
+import android.support.annotation.ColorInt;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +18,30 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        hideBottomNavBar();
+
+        Button login = findViewById(R.id.btn_accedi);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Bacheca.class));
+            }
+        });
     }
+
+    @Override
+    protected void onResume() {
+        hideBottomNavBar();
+        super.onResume();
+    }
+
+    private void hideBottomNavBar(){
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE         // immersive
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+        );
+    }
+
 }
