c2dc7ed86c7fd542d4029fafd61d32957cb04cc3
DavMrc <45066379+DavMrc@users.noreply.github.com>
2018-12-23 13:57:27 +0100

    tried adding booleans on OthersProfile to simplify, but it's crap

--- app/src/main/java/com/example/utente/fotogram/OthersProfile.java
+++ app/src/main/java/com/example/utente/fotogram/OthersProfile.java
@@ -3,7 +3,6 @@ package com.example.utente.fotogram;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,18 +14,17 @@ import com.example.utente.fotogram.Object_classes.Model;
 import com.example.utente.fotogram.Object_classes.ServerService;
 import com.example.utente.fotogram.Object_classes.User;
 
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-
 public class OthersProfile extends AppCompatActivity {
 
-    private User user;
+    private User otherUser;
     private Model m;
     private ImageHandler imageHandler;
     private ServerService serverService;
 
     private Button followButton;
+
+    private boolean is_self_profile;
+    private boolean following;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +34,31 @@ public class OthersProfile extends AppCompatActivity {
         m= Model.getInstance();
         imageHandler= new ImageHandler(this);
         serverService= ServerService.getInstance(this);
-        user= m.getOtherUser();
+        otherUser = m.getOtherUser();
 
-        getSupportActionBar().setTitle(user.getUsername());
+        is_self_profile = otherUser.getUsername().equals(m.getActiveUserNickname());
+        following= m.getActiveUserFriends().containsKey(otherUser.getUsername()) & ! is_self_profile;
+
+        getSupportActionBar().setTitle(otherUser.getUsername());
 
         // img profilo
         ImageView profilePic= findViewById(R.id.img_profile_pic);
-        Bitmap bitmap= imageHandler.decodeString(user.getImg());
+        Bitmap bitmap= imageHandler.decodeString(otherUser.getImg());
         if(bitmap != null) {
             profilePic.setImageBitmap(bitmap);
         }
 
         // username
         TextView username= findViewById(R.id.txt_username);
-        username.setText(user.getUsername());
+        username.setText(otherUser.getUsername());
 
         // bottone segui
         followButton= findViewById(R.id.btn_segui);
 
-        // se l'utente è gia nella lista degli amici
-        if( m.getActiveUserFriends().containsKey(user.getUsername()) ){
-            followButton.setText(R.string.following);
-        } // se il profilo selezionato è sè stessi
-        if( user.getUsername().equals(m.getActiveUserNickname()) ){
+        if(is_self_profile){
             followButton.setText(R.string.follow_yourself);
+        }else if( following ){
+            followButton.setText(R.string.following);
         }else{
             followButton.setText(R.string.not_following);
         }
@@ -73,18 +72,16 @@ public class OthersProfile extends AppCompatActivity {
     }
 
     private void followUnfollow() {
-        // se non si sta già seguendo l'utente
-        if (! m.getActiveUserFriends().containsKey(user.getUsername())) {
-            followButton.setText(R.string.following);
-            serverService.follow(m.getSessionID(), user.getUsername(), user.getImg());
-            Toast.makeText(this, "Hai iniziato a seguire " + user.getUsername(), Toast.LENGTH_SHORT).show();
-        } // se l'utente è sè stessi
-        if( m.getActiveUserFriends().containsKey(user.getUsername()) ){
+        if( is_self_profile ){
             Toast.makeText(this, R.string.follow_yourself, Toast.LENGTH_SHORT).show();
-        }else{
+        }else if( following ) {
             followButton.setText(R.string.not_following);
-            serverService.unfollow(m.getSessionID(), user.getUsername());
-            Toast.makeText(this, "Non segui più "+user.getUsername(), Toast.LENGTH_SHORT).show();
+            serverService.unfollow(m.getSessionID(), otherUser.getUsername());
+            Toast.makeText(this, "Non segui più "+ otherUser.getUsername(), Toast.LENGTH_SHORT).show();
+        }else{
+            followButton.setText(R.string.following);
+            serverService.follow(m.getSessionID(), otherUser.getUsername(), otherUser.getImg());
+            Toast.makeText(this, "Hai iniziato a seguire " + otherUser.getUsername(), Toast.LENGTH_SHORT).show();
         }
     }
 }
